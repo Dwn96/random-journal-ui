acb676dd59cf36a205f40d433edc1b31c6135fa4
D.njeru <wambu43@gmail.com>
2024-02-16 11:45:29 +0300

    Ft: Added journals landing page

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import NavBar from './Navbar/navbar'
+import LandingPage from './LandingPage/LandingPage'
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <LandingPage/>
+    </div>
+  )
+}
+
+export default App
